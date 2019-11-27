377349b78ad5a009e97e032aade628b71540e143
Vegemil <euna501@gmail.com>
2019-11-27 18:30:50 +0900

    Q10172

---
+++ Vegemil/Grass/01Step/Question/Q10172.cpp
@@ -0,0 +1,20 @@
+#include "Q10172.h"
+#include <iostream>
+
+void Q10172::start()
+{
+	std::cout << "|\\_/|" << std::endl;
+	std::cout << "|q p|   /}" << std::endl;
+	std::cout << "( 0 )\"\"\"\\" << std::endl;
+	std::cout << "|\"^\"`    |" << std::endl;
+	std::cout << "||_/=\\\\__|" << std::endl;
+
+
+// | \_/|
+// |q p|   /}
+// (0)"""\
+// |"^"`    |
+// || _ /= \\__ |
+
+}
+
--- Vegemil/Grass/01Step/main.cpp
+++ Vegemil/Grass/01Step/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+
 //#include "Question/Q2557.h"
 //#include "Question/Q10718.h"
-#include "Question/Q10171.h"
+//#include "Question/Q10171.h"
+#include "Question/Q10172.h"
 
 int main(void)
 {
 	//Q2557::start();
 	//Q10718::start();
-	Q10171::start();
+	//Q10171::start();
+	Q10172::start();
+
 	return 0;
 }
