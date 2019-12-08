b8b302f51fbc86c92309523e261f117213da16c7
Minji <so8376@gmail.com>
2019-12-08 22:37:51 +0900

    2주차
    1000, 1001, 10998, 7287

---
+++ Haru/Grass/Step01/1001.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+
+	scanf("%d %d", &num1, &num2);
+	printf("%d", num1 - num2);
+
+	return 0;
+}
---
+++ Haru/Grass/Step01/7287.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main()
+{
+	printf("29\n");
+	printf("plaming123");
+
+	return 0;
+}
