f560251429c8ad33f9dc8778a14c313d4451b7de
으나 :) <euna501@gmail.com>
2019-12-15 22:26:53 +0900

    Q10869

---
+++ Vegemil/Grass/01Step/Question/Q10869.cpp
@@ -0,0 +1,13 @@
+#include "Q10869.h"
+
+void Q10869::start()
+{
+	int a, b;
+	std::cin >> a >> b;
+
+    std::cout << a + b << std::endl; 
+    std::cout << a - b << std::endl; 
+    std::cout << a * b << std::endl; 
+    std::cout << a / b << std::endl; 
+    std::cout << a % b << std::endl; 
+}
---
+++ Vegemil/Grass/01Step/Question/Q10869.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+class Q10869
+{
+public:
+    static void start();
+};
